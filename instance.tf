fb03d08dfaf6ed84709e3bc7b874bc01593dea00
atlurisatheesh <atlurisatheesh93@gmail.com>
2024-05-21 18:48:17 +0530

    Auto push

--- instance.tf
+++ instance.tf
@@ -1,18 +1,18 @@
 provider "aws" {
   region = "us-east-1"
 }
-# resource "aws_instance" "demo" {
-#    ami = "ami-04b70fa74e45c3917"
-#    instance_type = "t2.micro"
+resource "aws_instance" "demo" {
+   ami = "ami-04b70fa74e45c3917"
+   instance_type = "t2.micro"
   
-# }
-
-resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-bucket"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
 }
+
+# resource "aws_s3_bucket" "example" {
+#   bucket = "my-tf-test-bucket"
+
+#   tags = {
+#     Name        = "My bucket"
+#     Environment = "Dev"
+#   }
+# }
 
