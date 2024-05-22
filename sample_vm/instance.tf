5bd5498f20a466676155fe92d13b40fbaf371684
atlurisatheesh <atlurisatheesh93@gmail.com>
2024-05-22 08:25:36 +0530

    Auto push

--- sample_vm/instance.tf
+++ sample_vm/instance.tf
@@ -6,3 +6,12 @@ resource "aws_instance" "web" {
     Name = "tef_prac"
   }
 }
+
+resource "aws_instance" "app" {
+  ami           = "ami-0e001c9271cf7f3b9"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "tef_demo"
+  }
+}
